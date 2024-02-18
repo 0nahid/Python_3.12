49d2eb5dba237016d94dce93f17e4c9646c845d9
0nahid <nahidhassanbulbul@gmail.com>
2024-02-18 17:21:37 -0500

    added loops

--- loops.py
+++ loops.py
@@ -9,14 +9,48 @@ x = 10
 #     print(x)
 #     x = x - 1
 
-x = 10
-for i in range(10, 0, -1): # here 10 is the start, 0 is the end and -1 is the step
-    print(i)
+# x = 10
+# for i in range(10, 0, -1): # here 10 is the start, 0 is the end and -1 is the step
+#     print(i)
     
+# loop through a list
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+
+# loop through a string
+# for x in "banana":
+#     print(x)
+
+# break statement
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#         break
+
+# continue statement
+# for x in fruits:
+#     if x == "banana":
+#         continue
+#     print(x)
+    
+# range() function
+# for x in range(6):
+#     print(x)
+    
+# for x in range(2, 7,2):
+#     print(x)
+
+# else in for loop
+# for x in range(6):
+#     print(x)
+# else:
+#     print("Finally finished!")
 
 
-# 0.6869761943817139
-# 0.7406010627746582
-# 0.6910970211029053
-# 0.7653689384460449
-# 0.6037797927856445
+# nested loops
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+# for x in adj:
+#     for y in fruits:
+#         print(x, y)
